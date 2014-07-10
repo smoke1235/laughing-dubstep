252b91be87c68a245e7dc55d70570d8f9deee2a0
Peter <oranjepeter@hotmail.com>
2014-07-10 11:15:36 +0200

    in

---
+++ src/plugins.js
@@ -0,0 +1,12 @@
+define([
+	"./plugins/transit",
+	"./plugins/form",
+	"./plugins/dubbletab",
+	"./plugins/rotate",
+	"./plugins/ui",
+	"./plugins/mobiscroll"
+], function( jQuery ) {
+
+return jQuery;
+
+});
